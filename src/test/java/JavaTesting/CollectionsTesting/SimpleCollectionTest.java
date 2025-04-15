25f0c77b0fdeaff6097b23f3f24acdeeaf6edccc
Alexandra1055 <agonzalez1055@alumnes.politecnicllevant.cat>
2025-04-15 20:03:06 +0200

    Pruebas con la interficie List, metodo prueba usando: removeAll, addAll,etc

--- src/test/java/JavaTesting/CollectionsTesting/SimpleCollectionTest.java
+++ src/test/java/JavaTesting/CollectionsTesting/SimpleCollectionTest.java
@@ -18,7 +18,91 @@ public class SimpleCollectionTest {
         for ( String s : nombres ){
             numerales.add(s);
         }
+
+        numerales.add("cuatro");
+        assertTrue(numerales.size() == 4);
+
         List<String> listaNumeros = Arrays.asList(nombres);
+
+        //listaNumeros.add("cuatro");
+
         assertTrue(listaNumeros.size() == 4);
+    }
+
+    @Test
+    public void diasSemana(){
+
+        List<String> diasSemana = new ArrayList<>();
+        List<String> diasLaborales = new ArrayList<>() ;
+        List<String> diasFestivos = new ArrayList<>() ;
+        List<String> diasLaboralesImpares = new ArrayList<>();
+        List<String> calculoDias = new ArrayList<>();
+
+        initDiasLaborales(diasLaborales);
+
+        assertTrue(diasLaborales.size() == 5);
+
+        assertEquals(0,diasSemana.size());
+        assertFalse(diasSemana.containsAll(diasLaborales));
+
+        diasSemana.addAll(diasLaborales);
+
+        assertEquals(5,diasSemana.size());
+        assertTrue(diasSemana.containsAll(diasLaborales));
+
+        diasFestivos.add("sabado");
+        diasFestivos.add("domingo");
+
+        diasSemana.addAll(diasFestivos);
+
+        assertEquals(7,diasSemana.size());
+        assertTrue(diasSemana.containsAll(diasFestivos));
+
+        List<String> diasImpares = new ArrayList<>();
+
+        diasImpares.addAll(diasSemana);
+
+        assertTrue(diasImpares.contains("martes"));
+
+        diasImpares.remove(1);
+
+        assertFalse(diasImpares.contains("martes"));
+
+        diasImpares.remove("jueves");
+
+        assertFalse(diasImpares.contains("jueves"));
+
+        diasLaboralesImpares.addAll(diasImpares);
+
+        diasLaboralesImpares.removeAll(diasFestivos);
+
+        assertEquals(3 ,diasLaboralesImpares.size());
+
+        assertFalse(diasLaboralesImpares.contains("sabado"));
+
+        calculoDias.addAll(diasSemana);
+
+        assertTrue(calculoDias.containsAll(diasLaborales));
+        assertTrue(calculoDias.containsAll(diasFestivos));
+
+        calculoDias.clear();
+
+        assertTrue(calculoDias.isEmpty());
+
+        calculoDias.addAll(diasSemana);
+
+        calculoDias.removeAll(diasFestivos);
+
+        assertEquals(5, calculoDias.size());
+
+    }
+
+    public void initDiasLaborales (List<String> dias){
+        dias.add("lunes");
+        dias.add("martes");
+        dias.add("miercoles");
+        dias.add("jueves");
+        dias.add("viernes");
+
     }
 }
