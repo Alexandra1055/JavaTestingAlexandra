04fb3e14bfadfbc7b8ca07b245ca4d15d1031ca2
Alexandra1055 <agonzalez1055@alumnes.politecnicllevant.cat>
2025-04-17 00:31:05 +0200

    Vemos @Rule y sus funciones, asi como expect y expectMessage

--- src/test/java/JavaTesting/ExplorandoJUnit/ExplorandoJUnitTest.java
+++ src/test/java/JavaTesting/ExplorandoJUnit/ExplorandoJUnitTest.java
@@ -1,13 +1,32 @@
 package JavaTesting.ExplorandoJUnit;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import java.security.InvalidParameterException;
 
 public class ExplorandoJUnitTest {
+
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
+
     @Test (expected = IllegalArgumentException.class)
     public void testetandoIllegalAlgumentException(){
 
         throw new IllegalArgumentException();
+    }
+
+    @Test
+    public void illegalParametersTest(){
+        expected.expect(IllegalArgumentException.class);
+        throw new IllegalArgumentException();
+    }
+
+    @Test
+    public void mensageExepcionTest(){
+        expected.expect(IllegalArgumentException.class);
+        expected.expectMessage("the");
+
+        throw new IllegalArgumentException("the exception");
     }
 }
