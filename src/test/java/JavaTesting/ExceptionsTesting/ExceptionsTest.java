634669e5c840289d147523b494bad57103e8c2d6
Alexandra1055 <agonzalez1055@alumnes.politecnicllevant.cat>
2025-04-17 00:17:50 +0200

    metodo de excepcion erronea

--- src/test/java/JavaTesting/ExceptionsTesting/ExceptionsTest.java
+++ src/test/java/JavaTesting/ExceptionsTesting/ExceptionsTest.java
@@ -71,4 +71,22 @@ public class ExceptionsTest {
 
     }
 
+    @Test(expected = NullPointerException.class)
+    public void excepcionErronea(){
+
+        Integer edad = null;
+        String edadEnLetras= "";
+        String texto;
+
+        try{
+            System.out.println("Paso 1: generamos NullPointerException");
+            edadEnLetras = edad.toString();
+            throw new IllegalArgumentException("Excepcion creada y lanzada por mi");
+        }catch ( IllegalArgumentException e ){
+            System.out.println("Paso 2: Capturamos la IllegalArgumentException");
+        } finally {
+            System.out.println("Paso final: codigo de cierre");
+        }
+
+    }
 }
