5199750e9a2bfacc7417c1958c316df1177f0061
Alexandra1055 <agonzalez1055@alumnes.politecnicllevant.cat>
2025-04-17 00:05:15 +0200

    nuevo metodo con fainally

--- src/test/java/JavaTesting/ExceptionsTesting/ExceptionsTest.java
+++ src/test/java/JavaTesting/ExceptionsTesting/ExceptionsTest.java
@@ -31,4 +31,23 @@ public class ExceptionsTest {
         assertEquals(""+18,edadEnLetras);
     }
 
+    @Test
+    public void tryCatchNullPointerTest(){
+
+        Integer edad = null;
+
+        String edadEnLetras= "";
+        String texto;
+
+        try{
+            edadEnLetras = edad.toString();
+        }catch ( NullPointerException npe ){
+            edad = 18;
+            edadEnLetras = edad.toString();
+        } finally {
+            texto = " Edad == 18 " + edadEnLetras;
+        }
+
+        assertEquals(""+18,texto);
+    }
 }
